37fd1361785ad944a12bba25996adada82ffaf0d
umarsheikh865-dot <umarsheikh865@gmail.com>
2026-05-31 11:50:32 +0100

    sumoftwonumbes

--- src/main/java/sumoftwonumbes.java
+++ src/main/java/sumoftwonumbes.java
@@ -1,8 +1,20 @@
 import java.util.Scanner;
+
 public class sumoftwonumbes {
-    Scanner input=new Scanner(System.in);
-    
-    
-    
-    int n
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter first number: ");
+        int num1 = input.nextInt();
+
+        System.out.print("Enter second number: ");
+        int num2 = input.nextInt();
+
+        int sum = num1 + num2;
+
+        System.out.println("Sum = " + sum);
+
+        input.close();
+    }
 }
