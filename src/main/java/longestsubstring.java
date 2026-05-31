27af470f26c03580e558780ad1a8b7a79aa70cba
umarsheikh865-dot <umarsheikh865@gmail.com>
2026-05-31 12:37:29 +0100

    longestsubstring

--- src/main/java/longestsubstring.java
+++ src/main/java/longestsubstring.java
@@ -1,12 +1,28 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+import java.util.HashSet;
 
-/**
- *
- * @author ftc
- */
 public class longestsubstring {
-    
+
+    public static void main(String[] args) {
+
+        String s = "abcabcbb";
+
+        HashSet<Character> set = new HashSet<>();
+
+        int left = 0;
+        int maxLength = 0;
+
+        for (int right = 0; right < s.length(); right++) {
+
+            while (set.contains(s.charAt(right))) {
+                set.remove(s.charAt(left));
+                left++;
+            }
+
+            set.add(s.charAt(right));
+
+            maxLength = Math.max(maxLength, right - left + 1);
+        }
+
+        System.out.println("Longest Length = " + maxLength);
+    }
 }
