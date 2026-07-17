5825a77e750487fac9c8ba292f1d661a30ff4325
umar <umarsheikh865@gmail.com>
2026-07-17 18:54:35 +0500

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,15 @@
+﻿using ConsoleApp1;
+
+namespace practicecsharp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello, World!");
+            //Addition.AdditionFun();
+            //SICALc.SIFun();
+            VariableCons.Fun();
+        }
+    }
+}
---
+++ ConsoleApp1/VariableCons.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Reflection.Metadata;
+
+namespace ConsoleApp1
+{
+    internal class VariableCons
+    {
+        const int Y = 200;
+        static int x = 100; //global variable
+
+        public static void Fun()
+        {
+            //int x = 10;
+            //x = 20;
+            //x = 30;
+            //Console.WriteLine("x=" + x);
+            //Console.WriteLine("Y=" + Y);
+
+            int x = 100;
+            float y = 2.345F;
+            string s = "hello";
+            double d = 422222222.456;
+            bool bl = true;
+            Console.WriteLine($"x={x} y={y} s = {s} bl = {bl} d= {d}");
+
+            Int16 x1 = 122;
+            Int64 x2 = 1222222222;
+            Double dl = 4555555.23;
+            Boolean bl2 = true;
+            Console.WriteLine($"x1={x1} x2={x2} dl = {dl} bl = {bl2} d= {bl2}");
+
+            object obj = null; // Corrected: Declared and assigned the object
+        }
+    }
+} // Corrected: Balanced closing braces
